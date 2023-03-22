8722f294adbb05606447f00a7b64cac45c6ff03b
vladian95 <kvankvan1995@gmail.com>
2023-03-22 14:19:49 +0300

    works

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,50 +9,6 @@ import { experiences } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { textVariant } from '../utils/motion';
 
-// const ExperienceCard = ({ experience }) => (
-//   <VerticalTimelineElement
-//     contentStyle={{ background: '#1d1836', color: '#fff' }}
-//     contentArrowStyle={{ borderRight: '7px solid #232631' }}
-//     date={experience.date}
-//     iconStyle={{ background: experience.iconBg }}
-//     icon={
-//       <div className="flex justify-center items-center w-full h-full">
-//         <img
-//           src={experience.icon}
-//           alt={experience.company_name}
-//           className="w-[60%] h-[60%] object-contain"
-//         />
-//       </div>
-//     }
-//   >
-//     <div>
-//       <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
-//       <p>{experience.company_name}</p>
-//     </div>
-//   </VerticalTimelineElement>
-// );
-
-// const Experience = () => {
-//   return (
-//     <>
-//       <motion.div variants={textVariant()}>
-//         <p className={styles.sectionSubText}>What i have done so far</p>
-//         <h2 className={styles.sectionHeadText}>Work Experience</h2>
-//       </motion.div>
-
-//       <div className="mt-20 flex flex-col">
-//         <VerticalTimeline>
-//           {experiences.map((experience, index) => (
-//             <ExperienceCard key={index} experience={experience} />
-//           ))}
-//         </VerticalTimeline>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default SectionWrapper(Experience, 'work');
-
 const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
@@ -101,12 +57,8 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
-        </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
-        </h2>
+        <p className={`${styles.sectionSubText} `}>What I have done so far</p>
+        <h2 className={`${styles.sectionHeadText} `}>Work Experience.</h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
